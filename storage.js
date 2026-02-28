35d52c5a57d26967738ff9a8d84c18933acb0ee0
Larissa Bertaze <larissabertaze@gmail.com>
2026-02-28 14:21:16 -0300

    Mapa dos Sonhos 🌍

--- storage.js
+++
@@ -1,9 +0,0 @@
-function getDreams() {
-  return JSON.parse(localStorage.getItem("dreams")) || [];
-}
-
-function saveDream(dream) {
-  const dreams = getDreams();
-  dreams.push(dream);
-  localStorage.setItem("dreams", JSON.stringify(dreams));
-}
